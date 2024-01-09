76322fc90f7b9d95fa050bfb9c41f45d29e536f3
Aya <aya.ashraf2015@hotmail.com>
2024-01-09 18:10:47 +0200

    Session 11 TASK

--- app/Http/Controllers/Auth/LoginController.php
+++ app/Http/Controllers/Auth/LoginController.php
@@ -37,4 +37,12 @@ class LoginController extends Controller
     {
         $this->middleware('guest')->except('logout');
     }
+
+    public function credentials(Request$request){
+        if(is_numeric($request->email)){
+        return['mobile'=>$request->email, 'password'=>$request->password];
+        }elseif(filter_var($request->email, FILTER_VALIDATE_EMAIL)){
+        return['email'=>$request->email, 'password'=>$request->password];
+        }
+        }
 }
